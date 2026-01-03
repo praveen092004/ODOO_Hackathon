b5480e92a736d022a452ab536dd923ed63e1add9
Praveen S <praveenselvaraj2004@gmail.com>
2026-01-03 13:23:20 +0530

    Tailwindcss impoted

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
-
+import Login from "./Modules/Login"
 import './App.css'
 
 function App() {
 
   return (
     <>
-      
+    <h2 className="text-red-500">Hello</h2>
+      <Login />
     </>
   )
 }
---
+++ frontend/src/Modules/Login.jsx
@@ -0,0 +1,11 @@
+
+
+const Login = () => {
+  return (
+    <>
+        <h2 className="bg-black text-white">Login Page</h2>
+    </>
+  )
+}
+
+export default Login
