9e7c801c6165ca5aafee7bb7cf34f884ea84117e
Praveen S <praveenselvaraj2004@gmail.com>
2026-01-03 13:58:59 +0530

    "modified"

--- frontend/src/Modules/Login.jsx
+++ frontend/src/Modules/Login.jsx
@@ -1,10 +1,14 @@
-
+import img1 from "../../public/assert/Login/profile.png"
 
 const Login = () => {
   return (
     <>
         <div>
-          <img src="" alt="" />
+          <img src={img1} alt="image not found" className="Login_profile_img" />
+        </div>
+
+        <div>
+          <input type="text" placeholder="Username" className="border 2px rounded-md m-4 p-2 focus:outline-none "/>
         </div>
     </>
   )
